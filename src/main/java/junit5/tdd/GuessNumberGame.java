5a07df0ab47aa0d398267ec96d3fec3d6797efe6
96197 <961977096@qq.com>
2020-07-23 19:54:25 +0800

    [Gavin] test:add should_return_4A0B_when_guess_number_given_1234 test

---
+++ src/main/java/junit5/tdd/GuessNumberGame.java
@@ -0,0 +1,7 @@
+package junit5.tdd;
+
+public class GuessNumberGame {
+    public String guess() {
+        return null;
+    }
+}
---
+++ src/test/java/junit5/tdd/GuessNumberGameTest.java
@@ -0,0 +1,18 @@
+package junit5.tdd;
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GuessNumberGameTest {
+    @Test
+    void should_return_4A0B_when_guess_number_given_1234(){
+        //given
+        GuessNumberGame guessNumberGame = new GuessNumberGame();
+        //when
+        String result=guessNumberGame.guess();
+        //then
+        assertEquals("4A0B",result);
+    }
+}

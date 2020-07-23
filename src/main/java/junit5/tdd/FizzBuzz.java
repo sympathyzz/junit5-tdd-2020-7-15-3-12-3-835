3ce4765f035b544b3a00861f1609c84fd7b36163
GAOGA4 <961977096@qq.com>
2020-07-23 11:59:00 +0800

    [Gavin] feature:implement should_return_fizzbuzz_when_play_fizz_buzz_given_15 test

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -2,6 +2,9 @@ package junit5.tdd;
 
 public class FizzBuzz {
     public String play(int number){
+        if(number%3==0&&number%5==0){
+            return "fizzbuzz";
+        }
         if(number%3==0){
             return "fizz";
         }

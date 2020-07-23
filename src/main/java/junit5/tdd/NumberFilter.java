81d684aa174186e684de90a1d26bcc7272d32729
96197 <961977096@qq.com>
2020-07-23 21:50:58 +0800

    [Gavin] test:implement should_return_pass_input_when_filter_number_given_5678 test

--- src/main/java/junit5/tdd/NumberFilter.java
+++ src/main/java/junit5/tdd/NumberFilter.java
@@ -7,9 +7,19 @@ public class NumberFilter {
     }
 
     public String filter() {
-        if(input.equals("1234")){
-            return "pass";
+        if(input.length()!=4 || isCharRepeat(input)){
+            return "Wrong Input,Input again";
         }
-        return "Wrong Input,Input again";
+        return "Pass";
+    }
+    public boolean isCharRepeat(String s){
+        boolean[] bool = new boolean[256];
+        for(byte a:s.getBytes()){
+            if(bool[a])
+                return true;
+            else
+                bool[a] = true;
+        }
+        return false;
     }
 }
